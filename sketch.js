5ba24abdb068aa3024ba567175ca301fb52ff011
MAO22nd <129629549+MAO22nd@users.noreply.github.com>
2023-07-20 19:06:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,76 +1,75 @@
-
-
-var plane_opitions={
-	
-	isStatic = true 
-}
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-  background("black");
-engine.update(engine);
-function preload()
-{
+
+var engine, world;
+
+var block1,block2,block3;
+
+
+function setup(){
+    var canvas = createCanvas(550,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    //criado corpos do plano e bloco
+    var plane_options={
+      isStatic: true
+    }
+
+    plane = Bodies.rectangle(600,580,1200,2,plane_options);
+    World.add(world,plane);
+  
+    //criados múltiplos corpos de blocos 
+    var block1_options = {
+      restitution:0.5,
+      friction:0.02,
+      frictionAir:0
+    }
+
+    var block2_options = {
+      restitution:0.7,
+      friction:0.01,
+      frictionAir:0.1
+    }
+
+    var block3_options = {
+      restitution:0.01,
+      friction:1,
+      frictionAir:0.3
+    }
+
+    block1 = Bodies.circle(220,10,10,block1_options);
+    World.add(world,block1);
+    
+    block2 = Bodies.rectangle(110,50,10,10,block2_options);
+    World.add(world,block2);
+    
+    block3 = Bodies.rectangle(350,50,10,10,block3_options);
+    World.add(world,block3);
+    
+    
+    //estilizando os corpos aqui
+    fill("brown");
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
 
 }
 
-function setup() {
-	createCanvas(800, 700);
+function draw(){
+    //Ajuste o tamanho da tela e mude a cor de fundo (background).
+    background("lightgreen");
+    //Chame update() para atualizar o motor de física em draw()
+    Engine.update(engine);
 
+  //criada forma para o plano e suporte
+  rect(plane.position.x,plane.position.y,1200);
+  
 
-	engine = Engine.create();
-	world = engine.world;
+  //criadas formas para todas as partículas
+  ellipse(block1.position.x,block1.position.y,30);
+  rect(block2.position.x,block2.position.y,50,50)
+  rect(block3.position.x,block3.position.y,100,50)
 
-	//Crie os Corpos Aqui.
-var block_1_opitions = {
-	restution:0.5,
-		friction:0.02,
-frictionAir:0
-
-}
-var block_2_opitions = {
-	restution:0.7,
-		friction:0.01,
-frictionAir:0.1
-
-}
-var block_3_opitions = {
-	restution:0.01,
-		friction:1,
-frictionAir:0.3
-
-}
-
-
-
-
-
-
-
-
-
-
-
-	
-block1= Bodies.circle(220,10,10,block1_opitions)
-Word.add(word,block1);
-	block2= Bodies.circle(110,50,10,10,block2_opitions)
-Word.add(word,block2);
-block3= Bodies.circle(350,50,10,10,block3_opitions)
-Word.add(word,block);
-	Engine.run(engine);
   
 }
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
-}
-
-
-
